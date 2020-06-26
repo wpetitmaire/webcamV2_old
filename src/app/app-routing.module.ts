518cc02449e81a7f69ec4b5130ab25c813402317
willy[MAC] <w.petitmaire@gmail.com>
2020-06-27 00:03:30 +0200

    Utilisation materialize-css + Ajout image test webcam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WebcamComponent } from './webcam/webcam.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'webcam',
+        component: WebcamComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'webcam'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
