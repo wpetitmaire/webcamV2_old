b9d9feff6cdbcb65f85b7b294f6bc4bae572ec27
willy[MAC] <w.petitmaire@gmail.com>
2020-06-26 21:02:34 +0200

    Création composant webcam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebcamComponent } from './webcam/webcam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebcamComponent
   ],
   imports: [
     BrowserModule,
